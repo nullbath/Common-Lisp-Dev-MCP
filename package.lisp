b048ed5eb931bf57019e189b959587e69ab0eda3
nullbath <nullbath@gmail.com>
2025-08-29 20:07:41 -0500

    feat(package.lisp): add cl to commonlispmcp package
    Added the #:cl to the defpackage directive in the commonlispmcp
    package in package.lisp.

--- package.lisp
+++ package.lisp
@@ -1,11 +1,3 @@
 ;;;; package.lisp
 (defpackage #:commonlispmcp
-  (:use #:cl
-	)
-  (:import-from #:40ants-mcp/content/text
-                #:text-content)
-  (:import-from #:openrpc-server))
-
-;; (defpackage #:commonlispmcp
-;;   (:use #:cl
-;; 	))
+  (:use #:cl))
